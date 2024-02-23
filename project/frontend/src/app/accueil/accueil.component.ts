5be070e671c6f57bef12b0829e8572ad51af9a0b
Viggo Casciano <viggo.casciano@ensto.com>
2024-02-23 15:56:26 +0100

    accueil: j'ai fait la page d'accueil elle est presque finit.

--- project/frontend/src/app/accueil/accueil.component.ts
+++ project/frontend/src/app/accueil/accueil.component.ts
@@ -10,13 +10,29 @@ export class AccueilComponent implements OnInit {
 
   champions: any[] = [];
   selectedChampion: any = null;
+  championImages: string[] = [];
 
   constructor(private dataService: AccueilService) { }
 
   ngOnInit(): void {
     this.dataService.fetchData().subscribe(data => {
       this.champions = data.champions;
+
+      // Préchargement des images
+      this.champions.forEach(champion => {
+        this.preloadImage(champion.icon);
+        champion.skins.forEach((skin: any) => {
+          const imageUrl = Object.values(skin)[0] as string;
+          this.preloadImage(imageUrl);
+        });
+      });
     });
+  }
+
+  preloadImage(url: string): void {
+    const img = new Image();
+    img.src = url;
+    this.championImages.push(url); // Stockage de l'URL dans le tableau pour référence ultérieure
   }
 
   showSkins(champion: any) {
